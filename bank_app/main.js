5c6d1e39ff3f856c5aa419c0ecea0839c3475096
Jayanta Mondal <jayanta551905@gmail.com>
2024-04-05 00:40:56 +0600

    bank console app using js class

---
+++ bank_app/main.js
@@ -0,0 +1,46 @@
+class BankAccount {
+    constructor(accountNumber, ownerName, balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        console.log(`Deposited $${amount}. New balance: $${this.balance}`);
+    }
+
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log("Insufficient funds");
+        } else {
+            this.balance -= amount;
+            console.log(`Withdrawn $${amount}. New balance: $${this.balance}`);
+        }
+    }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    displayAccountInfo() {
+        console.log(`Account Number: ${this.accountNumber}`);
+        console.log(`Owner Name: ${this.ownerName}`);
+        console.log(`Balance: $${this.balance}`);
+    }
+}
+
+// Create BankAccount class object
+const accJayanta = new BankAccount(1001, "Jayanta Mondal", 5000);
+const accAjay = new BankAccount(1002, "Ajay Mondal", 1000);
+
+// call methods with parameters
+accJayanta.displayAccountInfo();
+accJayanta.deposit(2000);
+accJayanta.withdraw(1000);
+
+console.log("===========================");
+
+accAjay.displayAccountInfo();
+accAjay.deposit(500);
+accAjay.withdraw(1200);
